a11a834bee659e0420b46f5bb6f81fcfbe37b728
andres manuel hense <ahense@dc.uba.ar>
2021-08-07 23:46:39 -0300

    intro

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: 'Andres Hense Blog Post',
+      description: 'Philosophy and Essays',
       image:
         'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
